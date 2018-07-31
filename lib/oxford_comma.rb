4ba2773200ce6634d9d395ea48623d975b78c6ab
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-07-31 18:13:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     array.count == 2
     array.join(" and ")
   else
-    array.insert(-1, "and ")
+    array.insert(-2, "and ")
     return array[0..-2].join(", ") + array[-1]
   end
 end
