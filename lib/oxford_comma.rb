10f0c7eba61c4fc1fea66df4134feeb8540f5f9a
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-07-31 18:10:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 
-
 def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
+  if array.count == 1
+    return array[0]
+  elsif
+    array.count == 2
+    array.join(" and ")
+  else
+    return array[0..-2].join(", ") + "and" + array[-1]
   end
-  array.join(", ")
 end
